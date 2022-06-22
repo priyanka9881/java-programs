280afcb9e23023f0e50f4aef5a6c60520812cf8d
priyanka9881 <59731830+priyanka9881@users.noreply.github.com>
2022-06-22 20:34:44 +0500

    Delete Strings directory

--- Strings/ParseInt.java
+++
@@ -1,9 +0,0 @@
-public class ParseInt {
-    public static void main(String args[]){
-        String str = "123";
-
-        int number = Integer.parseInt(str);
-
-        System.out.println(number);
-    }    
-}
--- Strings/ToString.java
+++
@@ -1,9 +0,0 @@
-public class ToString {
-    public static void main(String args[]){
-        int number = 123;
-
-        String str = Integer.toString(number); 
-
-        System.out.println(str.length());
-    }    
-}
