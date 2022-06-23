449a9670d563db853ab1f22f01b24d519f61b0c4
priyanka9881 <59731830+priyanka9881@users.noreply.github.com>
2022-06-23 13:53:12 +0500

    Problems on String

---
+++ Strings/String2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Compare {
+    public static void main(String args[]){
+        String name1 = "tony";       
+        String name2 = "tony"; 
+        
+        if(name1.compareTo(name2)){
+
+        }
+    }   
+}
